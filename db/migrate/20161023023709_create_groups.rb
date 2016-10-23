7be04316bc20beb59d507b4c0b6d3edb78db8a9b
伊澤裕聖 <yusei@izawahiroshisei-no-MacBook-Pro.local>
2016-10-23 13:11:19 +0900

    Modify column setting

--- db/migrate/20161023023709_create_groups.rb
+++ db/migrate/20161023023709_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration
   def change
     create_table :groups do |t|
-      t.string :name null: false
+      t.string :name, null: false
       t.timestamps null: false
     end
   end
--- db/migrate/20161023025237_create_chats.rb
+++ db/migrate/20161023025237_create_chats.rb
@@ -3,8 +3,8 @@ class CreateChats < ActiveRecord::Migration
     create_table :chats do |t|
       t.string   :text
       t.string   :image
-      t.references :user :foreign_key :true
-      t.references :group :foreign_key :true
+      t.references :user, :foreign_key :true
+      t.references :group, :foreign_key :true
       t.timestamps null: false
     end
   end
