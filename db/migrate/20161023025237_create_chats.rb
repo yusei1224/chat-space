5ee2af0ceb30937b5a2ac036fc6c36587b1ccd99
伊澤裕聖 <yusei@izawahiroshisei-no-MacBook-Pro.local>
2016-10-23 12:03:21 +0900

    Add column to chats table

--- db/migrate/20161023025237_create_chats.rb
+++ db/migrate/20161023025237_create_chats.rb
@@ -1,7 +1,10 @@
 class CreateChats < ActiveRecord::Migration
   def change
     create_table :chats do |t|
-
+      t.string   :text
+      t.string   :image
+      t.references :user :foreign_key :true
+      t.references :group :foreign_key :true
       t.timestamps null: false
     end
   end
