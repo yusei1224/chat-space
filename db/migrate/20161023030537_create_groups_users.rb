b5b6adbb6636acada21486cc6566a9b638a75d54
伊澤裕聖 <yusei@izawahiroshisei-no-MacBook-Pro.local>
2016-10-23 12:09:51 +0900

    Add column to groups_users table

--- db/migrate/20161023030537_create_groups_users.rb
+++ db/migrate/20161023030537_create_groups_users.rb
@@ -1,7 +1,8 @@
 class CreateGroupsUsers < ActiveRecord::Migration
   def change
     create_table :groups_users do |t|
-
+      t.references :group :foreign_key :true
+      t.references :user :foreign_key :true
       t.timestamps null: false
     end
   end
